c98f3805be918ac26a4686f351a3406883791e89
Zac Clay <zacclay@gmail.com>
2023-06-29 11:25:59 -0400

    Catch localStorage errors from older versions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ErrorBoundary } from "react-error-boundary";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const clearLocalStorage = () => {
+  localStorage.clear();
+};
+function fallbackRender({ error }) {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold mb-6">Something went wrong</h1>
+          <div>
+            <code>{error.message}</code>
+          </div>
+          <button onClick={() => window.location.reload()} className="btn btn-error mt-6">Try reloading</button>
+        </div>
+      </div>
+    </div>
+  );
+}
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary fallbackRender={fallbackRender} onError={clearLocalStorage}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
